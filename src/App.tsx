023ed1d0c467ea514f2ec5a41ab5358fa72a4d55
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-04-28 13:43:30 +0000

    ca01133 chore: update TypeScript configuration and add new check scripts

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { createHashRouter, RouterProvider, Navigate } from 'react-router-dom'
+import CreateRoutePage from './components/CreateRoutePage'
+import ErrorPage from './components/ErrorPage'
+import React, { useState } from 'react'
+import './App.css'
+import PageWrapper from './components/PageWrapper'
+import EvaluationPage from './pages/EvaluationPage'
+import PredictionOverview from './features/predictions-overview/PredictionOverview'
+import SetChapUrl from './features/route-api/SetChapUrl'
+import Settings from './features/settings/Settings'
+
+
+const router = createHashRouter([
+    {
+        path: 'route',
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: 'create-route',
+                element: <CreateRoutePage />,
+            },
+        ],
+    },
+    {
+        path: '',
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to="/evaluate" replace />,
+            },
+            {
+                path: '/evaluate',
+                element: <PageWrapper component={<EvaluationPage />} />,
+            },
+            {
+                path: '/predict',
+                element: <PageWrapper component={<PredictionOverview />} />,
+            },
+            {
+                path: '/settings',
+                element: <PageWrapper component={<Settings />} />,
+            },
+        ],
+    },
+])
+
+const App = () => {
+    const [isLoadingRouteConfig, setIsLoadingRouteConfig] = useState(true)
+
+    return (
+        <>
+            <SetChapUrl setIsLoadingRouteConfig={setIsLoadingRouteConfig} />
+
+            {!isLoadingRouteConfig && (
+                <RouterProvider router={router}></RouterProvider>
+            )}
+        </>
+    )
+}
+
+export default App
