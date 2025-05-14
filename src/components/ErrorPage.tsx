0ac6195d77d92a57103c7d85c03c015224ced0b4
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-05-14 16:55:45 +0000

    35c911a feat: add new types and update model definitions

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import i18n from '@dhis2/d2-i18n'
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom'
+
+const ErrorPage = () => {
+    const error = useRouteError()
+
+    if (!isRouteErrorResponse(error)) {
+        return (
+            <div id="error-page">
+                <h1>{i18n.t('Oops!')}</h1>
+                <p>{i18n.t('Sorry, an unexpected error has occurred.')}</p>
+            </div>
+        )
+    }
+
+    return (
+        <div id="error-page">
+            <h1>{i18n.t('Oops!')}</h1>
+            <p>{i18n.t('Sorry, an unexpected error has occurred.')}</p>
+            <p>
+                <i>{error.statusText}</i>
+            </p>
+        </div>
+    )
+}
+
+export default ErrorPage
---
+++ src/features/settings/RouteSettings/RouteActions/RouteActions.tsx
@@ -0,0 +1,120 @@
+import React, { useRef, useState } from "react"
+import {
+    Button,
+    FlyoutMenu,
+    IconDelete16,
+    IconEdit16,
+    IconMore16,
+    IconShare16,
+    Layer,
+    MenuItem,
+    Popper,
+    SharingDialog,
+} from "@dhis2/ui"
+import i18n from "@dhis2/d2-i18n"
+import { EditRoute } from "../EditRoute"
+import type { Route } from "../../../../hooks/useRoute"
+import { DeleteRouteModal } from "../DeleteRouteModal"
+import { useQueryClient } from "@tanstack/react-query";
+import styles from "./RouteActions.module.css"
+
+type Props = {
+    route: Route
+}
+
+export const RouteActions = ({ route }: Props) => {
+    const queryClient = useQueryClient();
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    const [isSharingDialogOpen, setIsSharingDialogOpen] = useState(false)
+    const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
+    const popperRef = useRef<HTMLDivElement>(null)
+
+    return (
+        <>
+            <div className={styles.routeActions}>
+                <Button
+                    icon={<IconShare16 />}
+                    small
+                    onClick={() => setIsSharingDialogOpen(true)}
+                >
+                    {i18n.t('Sharing')}
+                </Button>
+
+                <div ref={popperRef}>
+                    <Button
+                        small
+                        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                    >
+                        <IconMore16 />
+                    </Button>
+                    {isDropdownOpen && (
+                        <Layer onBackdropClick={() => setIsDropdownOpen(false)}>
+                            <Popper
+                                placement="bottom-end"
+                                reference={popperRef}
+                            >
+                                <FlyoutMenu>
+                                    <MenuItem
+                                        dense
+                                        onClick={() => {
+                                            setIsEditDialogOpen(true)
+                                            setIsDropdownOpen(false)
+                                        }}
+                                        label={i18n.t('Edit')}
+                                        icon={<IconEdit16 />}
+                                    />
+                                    <MenuItem
+                                        dense
+                                        onClick={() => {
+                                            setIsSharingDialogOpen(true)
+                                            setIsDropdownOpen(false)
+                                        }}
+                                        label={i18n.t('Sharing')}
+                                        icon={<IconShare16 />}
+                                    />
+                                    <MenuItem
+                                        dense
+                                        destructive
+                                        onClick={() => {
+                                            setIsDeleteDialogOpen(true)
+                                            setIsDropdownOpen(false)
+                                        }}
+                                        label={i18n.t('Delete')}
+                                        icon={<IconDelete16 />}
+                                    />
+                                </FlyoutMenu>
+                            </Popper>
+                        </Layer>
+                    )}
+                </div>
+
+            </div>
+            {isSharingDialogOpen && (
+                <SharingDialog
+                    id={route.id}
+                    type="route"
+                    onClose={() => {
+                        queryClient.invalidateQueries({ queryKey: ['routes'] });
+                        setIsSharingDialogOpen(false)
+                    }}
+                />
+            )}
+
+            {isDeleteDialogOpen && (
+                <DeleteRouteModal
+                    routeId={route.id}
+                    onClose={() => setIsDeleteDialogOpen(false)}
+                />
+            )}
+
+            {isEditDialogOpen && (
+                <EditRoute
+                    route={route}
+                    onClose={() => setIsEditDialogOpen(false)}
+                />
+            )}
+        </>
+    )
+} 
