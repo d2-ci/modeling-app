0c634a16b1d659505cd857de69bb1459c20faa26
Birk Johansson <birk@dhis2.org>
2025-06-02 08:54:33 +0000

    6b48660 refactor: refactor to collapseSidebaer for clarity

---
+++ src/Globals.d.ts
@@ -0,0 +1,9 @@
+declare module '*.module.css'
+declare module 'react-dropzone'
+declare module '@dhis2/analytics'
+declare module 'file-saver'
+declare module 'jszip'
+declare module 'openapi-typescript-codegen'
+declare module '@dhis2/app-adapter'
+declare module '@dhis2/d2-i18n'
+
