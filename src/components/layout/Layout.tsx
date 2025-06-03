a826c447b0be02af842c1cef93ece844e6217ef2
Birk Johansson <birk@dhis2.org>
2025-06-03 21:14:51 +0000

    6dbff10 fix: move request file to src, exclude from ts

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,49 @@
+import cx from 'classnames'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Sidebar } from '../sidebar'
+import css from './Layout.module.css'
+
+export const ID_MAIN_LAYOUT = 'main-layout'
+interface BaseLayoutProps {
+    children: React.ReactNode
+    sidebar?: React.ReactNode
+}
+
+export const BaseLayout = ({ children, sidebar }: BaseLayoutProps) => {
+    return (
+        <div className={css.wrapper}>
+            {sidebar}
+            <div id={ID_MAIN_LAYOUT} className={css.main}>{children}</div>
+        </div>
+    )
+}
+
+export const SidebarLayout = ({
+    children,
+    hideSidebar,
+}: {
+    children: React.ReactNode
+    hideSidebar?: boolean
+}) => {
+    return (
+        <BaseLayout
+            sidebar={
+                <Sidebar
+                    hideSidebar={hideSidebar}
+                    className={cx(css.sidebar, { [css.hide]: hideSidebar })}
+                />
+            }
+        >
+            {children}
+        </BaseLayout>
+    )
+}
+
+export const Layout = () => {
+    return (
+        <SidebarLayout>
+            <Outlet />
+        </SidebarLayout>
+    )
+}
