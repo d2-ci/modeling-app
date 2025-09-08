ffeca60a38c7f1565fe6aa0320ca505981db008a
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-09-08 11:55:03 +0000

    4f3231f refactor: remove legacy evaluation components and related styles

---
+++ src/pages/PredictionsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import PredictionOverview from '../features/predictions-overview/PredictionOverview'
+
+const PredictionsPage = () => {
+    return (
+        <div>
+            <PredictionOverview />
+        </div>
+    )
+}
+
+export default PredictionsPage
